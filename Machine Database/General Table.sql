15416bbd278d8552248927089370a5950f2ad2d8
RichardTech2002 <73807694+RichardTech2002@users.noreply.github.com>
2022-06-28 17:28:42 +1200

    Added more table rows

---
+++ Machine Database/General Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[General Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [MachineName] NCHAR(25) NULL, 
+    [YearBuilt] INT NULL, 
+    [DonatedBy] NCHAR(25) NULL, 
+    [DonationDate] INT NULL, 
+    [Description] VARCHAR(1500) NULL
+)
