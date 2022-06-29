2949250fc8f9c0c3ba85473fd1da709a74bd2eff
RichardTech2002 <73807694+RichardTech2002@users.noreply.github.com>
2022-06-29 23:13:34 +1200

    Buttons now work.

--- User Interface For Vintage Club Database/Form1.cs
+++ User Interface For Vintage Club Database/Form1.cs
@@ -16,5 +16,22 @@ namespace User_Interface_For_Vintage_Club_Database
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 f3 = new Form3();
+            f3.ShowDialog();
+        }
     }
 }
